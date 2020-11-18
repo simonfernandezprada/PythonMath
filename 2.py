aab02c06657b29f00eee0c2bc667f0dae0d53c4b
Simon Fernandez-Prada Paz <simonfernandezprada@gmail.com>
2020-11-17 22:28:07 -0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,12 @@
+print('''
+(30% del puntaje total) Desarrollar un algoritmo que permita el ingreso de un valor numérico y lo clasifique en “primo” si únicamente es divisible por sí mismo y por la unidad o en “compuesto”, en el caso contrario. Si se digita 1, deberá informar que es la unidad y que no es ni primo ni compuesto. Si el número es compuesto, informará cuál es el divisor más pequeño que tiene, diferente de la unidad.
+El algoritmo permitirá la entrada cíclica de datos para su clasificación, hasta cuando el usuario digite
+un número menor que 1.
+El algoritmo se mostrará en su máquina en un diagrama de flujo elaborado en PeSint, DFD u otra aplicación equivalente, para ser evaluado así:
+a. El programa corre  10%
+b. El programa identifica correctamente el 1  10%
+c. El programa identifica correctamente si un número es primo  30%
+d. El programa muestra correctamente el divisor más pequeño de cualquier número compuesto 
+30%
+e. El programa es cíclico  20%
+''')
